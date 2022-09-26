31387ee1f4ce13c5f48a59a3d315751009e05416
Go Eunseon <goeunseon@Goui-iMac.local>
2022-09-26 13:36:04 +0900

    인증 - 네비게이션 가드

--- src/router/index.js
+++ src/router/index.js
@@ -8,10 +8,16 @@ import NotFound from '../components/NotFound'
 
 Vue.use(VueRouter)
 
+const requireAuth = (to, from, next) => {
+  const isAuth = localStorage.getItem('token')
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+  isAuth ? next() : next(loginPath)
+}
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: Home },
+    { path: '/', component: Home, beforeEnter: requireAuth },
     { path: '/login', component: Login },
     { path: '/board/:bid', component: Board, children:[
       {path: 'c/:cid', component: Card }
